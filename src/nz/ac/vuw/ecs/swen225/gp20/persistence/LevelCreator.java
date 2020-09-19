40982c603513c3851775baea0f06a3a787e4365f
Jared Boult <boultjare@myvuw.ac.nz>
2020-09-19 22:32:49 +1200

    Created level1.json and persistence package

---
+++ src/nz/ac/vuw/ecs/swen225/gp20/persistence/LevelCreator.java
@@ -0,0 +1,68 @@
+package nz.ac.vuw.ecs.swen225.gp20.persistence;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ *  Feeling cute might delete later
+ *  Temporary class helping me generate the level JSON files
+ */
+public class LevelCreator {
+
+    /**
+     * Constructor for LevelLoader.
+     */
+    LevelCreator(){
+        generateLevelOne();
+    }
+
+    /**
+     * Create the JSON file
+     */
+    private void generateLevelOne(){
+        String[] levelOneRawData = {"_____________________",
+                "_____________________",
+                "_____________________",
+                "_____/////_/////_____",
+                "_____/___///___/_____",
+                "_____/_#_/@/_#_/_____",
+                "___/////C/X/C/////___",
+                "___/_d_A_____B_d_/___",
+                "___/_#_/a_?_b/_#_/___",
+                "___/////#_!_#/////___",
+                "___/_#_/a___b/_#_/___",
+                "___/___B__#__A___/___",
+                "___//////D/D//////___",
+                "_______/__/__/_______",
+                "_______/__/#_/_______",
+                "_______/_#/c_/_______",
+                "_______///////_______",
+                "_____________________",
+                "_____________________",
+                "_____________________"
+        };
+        try {
+            Writer writer = Files.newBufferedWriter(Paths.get("levels/level1.json"));
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            gson.toJson(levelOneRawData, writer);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Generates the output file
+     * @param args
+     */
+    public static void main(String[] args){
+        new LevelCreator();
+    }
+}
---
+++ src/nz/ac/vuw/ecs/swen225/gp20/persistence/LevelLoader.java
@@ -0,0 +1,16 @@
+package nz.ac.vuw.ecs.swen225.gp20.persistence;
+
+import com.google.gson.Gson;
+
+/**
+ *  A class responsible for loading level data from json files.
+ */
+public class LevelLoader {
+
+    /**
+     * Constructor for LevelLoader.
+     */
+    LevelLoader(){
+
+    }
+}
