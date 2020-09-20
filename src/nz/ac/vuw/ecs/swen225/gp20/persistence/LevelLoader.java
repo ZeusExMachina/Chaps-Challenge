6051c53cd37ddfe62c006be769ae374ca2c3f60f
Jared Boult <boultjare@myvuw.ac.nz>
2020-09-20 20:38:14 +1200

    Will scan levels folder for JSON files

--- src/nz/ac/vuw/ecs/swen225/gp20/persistence/LevelLoader.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/persistence/LevelLoader.java
@@ -2,15 +2,48 @@ package nz.ac.vuw.ecs.swen225.gp20.persistence;
 
 import com.google.gson.Gson;
 
+import java.io.File;
+
 /**
  *  A class responsible for loading level data from json files.
  */
 public class LevelLoader {
 
     /**
+     * Stores the JSON files that hold level layout data
+     */
+    File[] levels;
+
+    /**
      * Constructor for LevelLoader.
      */
     LevelLoader(){
+        levels = detectLevelFiles();
+    }
 
+    /**
+     * Get JSON files from the levels folder.
+     * @return an Array of files that store the game levels
+     */
+    private File[] detectLevelFiles(){
+        File levelsFolder = new File("levels");
+        return levelsFolder.listFiles(((dir, name) -> name.endsWith(".json")));
+    }
+
+    /**
+     * Gets the number of levels in the game
+     * @return the number of levels
+     */
+    public int getNumberOfLevels(){
+        return levels.length;
+    }
+
+    /**
+     * Just for testing this class
+     * @param args string args
+     */
+    public static void main(String[] args){
+        LevelLoader test = new LevelLoader();
+        System.out.println("test");
     }
 }
